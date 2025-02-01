99bebb5175b4515aa45bbe375695e38420e86052
Krystek <krystek.sterna@gmail.com>
2025-02-01 20:40:49 +0100

    Bezkwadratowa sprawdzenie

--- liczba.cpp
+++ liczba.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
+bool czy_bezkwadratowa(int n) {
+    for(int i=2; i<(n/2)+1; i++) {
+        if(n%(i*i)==0) {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
     int liczba;
     cin >> liczba;
-    cout<< liczba << endl;
+    cout<< liczba << " czy bezkwadratowa: " << czy_bezkwadratowa(liczba) << endl;
     return 0;
 }
